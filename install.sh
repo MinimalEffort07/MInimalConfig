2c505f7337e5e2b829bc8f3a5533406e468d2279
Emmanuel Christianos <z5206513@unsw.edu.au>
2021-12-30 13:51:37 +1100

    feat: adding to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-cp ~/.tmux.conf backup/tmux.conf
+check_command() {
+
+if [ $1 -eq 0 ]; then
+  tput setaf 2; echo "["$2" SUCCESS]"
+else
+  tput setaf 1; echo "["$2" FAIL]"
+fi
+
+}
+var="Test ddd"
+check_command `cp ~/.tmux.conf backup/tmux.conf` $var
 cp ~/.vimrc backup/vimrc
 cp ~/.zshrc backup/zshrc
-cp -R ~/.oh-my-zsh backup/oh-my-zsh
+#cp -R ~/.oh-my-zsh backup/oh-my-zsh
 
 cp tmux.conf ~/.tmux.conf
 cp zshrc  ~/.zshrc
 cp vimrc ~/.vimrc
-cp -R oh-my-zsh  ~/.oh-my-zsh
+#cp -R oh-my-zsh  ~/.oh-my-zsh
 
