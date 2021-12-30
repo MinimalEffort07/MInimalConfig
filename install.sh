008fc5db49a4acb894e4a68c919d542258de8f5e
Emmanuel Chritianos <emmanuelchristianos@pm.me>
2021-12-30 19:23:59 +1100

    feat: added proper colour coding to error messages

--- install.sh
+++ install.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
+RESET='\033[0m'
+B_RED='\033[1;31m'
+B_GREEN='\033[1;32m'
+B_YELLOW='\033[1;33m'
+
 check_command() {
 
 if [ $1 -eq 0 ]; then
-  tput setaf 2; echo "["$2" SUCCESS]"
+  printf "${B_GREEN}[SUCCESS]${RESET} $2\n"
 else
-  tput setaf 1; echo "["$2" FAIL]"
+  printf "${B_RED}[FAIL]${RESET} $2\n"
 fi
 
 }
-var="Test ddd"
-check_command `cp ~/.tmux.conf backup/tmux.conf` $var
+
+cp ~/.tmux.conf backup/tmux.conf &>/dev/null 
+check_command $? "tmux.conf backup"
+
 cp ~/.vimrc backup/vimrc
-cp ~/.zshrc backup/zshrc
-#cp -R ~/.oh-my-zsh backup/oh-my-zsh
+check_command $? "vimrc backup"
+
+cp ~/.zshrc backup/zshrc &>/dev/null 
+check_command $? "zshrc backup"
+
+cp -R ~/.oh-my-zsh backup/oh-my-zsh
+check_command $? "oh-my-zsh backup"
 
 cp tmux.conf ~/.tmux.conf
+check_command $? "setting up tmux.conf"
+
 cp zshrc  ~/.zshrc
+check_command $? "setting up zshrc"
+
 cp vimrc ~/.vimrc
-#cp -R oh-my-zsh  ~/.oh-my-zsh
+check_command $? "setting up vimrc"
+
+cp -R oh-my-zsh  ~/.oh-my-zsh
+check_command $? "setting up oh-my-zsh"
 
